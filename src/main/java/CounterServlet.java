d08dedbfa7d81f39b17ba9c9d1410bd4543ff510
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-03-04 15:33:47 -0600

    welcome webapp

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int hitCount;
+
+    public void init(){
+        hitCount = 0;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        res.setContentType("text/html");
+
+        hitCount ++;
+
+        PrintWriter out = res.getWriter();
+
+        out.println("<h1> You are guest number: "+hitCount +"</h1>"
+        );
+    }
+}
