8840e24b8e98762dc65ddb914a889dc88593219c
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-03-12 09:42:37 -0600

    config and git ignore

---
+++ src/main/java/Config.java
@@ -0,0 +1,12 @@
+public class Config {
+        public String getUrl() {
+            return "jdbc:mysql://localhost/database_name?serverTimezone=UTC&useSSL=false";
+        }
+        public String getUser() {
+            return "root";
+        }
+        public String getPassword() {
+            return "user";
+        }
+
+}
